6af17aa66acfa15c3cffd0d377f6a768c399bfb9
Julio Dourado <juliodourado419@gmail.com>
2025-02-10 01:02:03 -0300

    mudancas de altura ao longo do array dp para formar os pontos-chave + passed + doc

--- skyline-problem/solucao.py
+++ skyline-problem/solucao.py
@@ -13,7 +13,16 @@ class Solution(object):
             for j in range(mapeamento[inicio], mapeamento[fim]):
                 if altura > dp[j]:
                     dp[j] = altura
-        return []
+        resposta = []
+        altura_atual = 0
+        for i in range(len(dp)):
+            if dp[i] != altura_atual:
+                resposta.append([conjunto_ordenado[i], dp[i]])
+                altura_atual = dp[i]
+        if altura_atual != 0:
+            resposta.append([conjunto_ordenado[-1], 0])
+        return resposta
 
 solucao = Solution()
 print(solucao.getSkyline([[2,9,10],[3,7,15],[5,12,12],[15,20,10],[19,24,8]]))
+print(solucao.getSkyline([[0,2,3],[2,5,3]]))
